2a35969c7826a72b8c5dab92a8c3204b35462766
Rohit <rohit.shinde12194@gmail.com>
2018-10-19 16:25:55 -0400

    Added login activity to log in users.

---
+++ app/src/main/java/entry/easyentry/activities/LoginActivity.java
@@ -0,0 +1,85 @@
+package entry.easyentry.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import entry.easyentry.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextLoginEmail)
+    EditText editTextEmail;
+
+    @BindView(R.id.editTextLoginPassword)
+    EditText editTextPassword;
+
+    @BindView(R.id.btnLoginSignin)
+    Button btnLogin;
+
+    private FirebaseAuth auth;
+    private static final String TAG = "LoginActivity";
+
+    @OnClick(R.id.btnLoginSignin)
+    void login(){
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: User successfully logged in");
+                            Toast.makeText(LoginActivity.this,"User logged in successfully",Toast.LENGTH_LONG).show();
+
+                            // Start Main Activity on successful login
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+                            FirebaseUser user = auth.getCurrentUser();
+                        }
+                        else{
+                            Log.d(TAG, "onComplete: User not logged in.");
+                            Toast.makeText(LoginActivity.this,"Check credentials again.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser!=null){
+            // Start MainActivity if the user is currently logged in.
+            Log.d(TAG, "onStart: User logged in. Starting MainActivity");
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
