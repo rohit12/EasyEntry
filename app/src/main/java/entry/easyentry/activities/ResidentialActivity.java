f612c3bfe331d571f837603b893928203f6cf3bc
Rohit <rohit.shinde12194@gmail.com>
2018-10-20 08:09:35 -0400

    Added feature. If visitor's number exists in database, he will not be asked to verify his number.

--- app/src/main/java/entry/easyentry/activities/ResidentialActivity.java
+++ app/src/main/java/entry/easyentry/activities/ResidentialActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -27,7 +28,13 @@ import entry.easyentry.utils.Utils;
 
 public class ResidentialActivity extends AppCompatActivity {
 
-    private Dao visitorDao;
+    //Flow for the activity:
+    //1. Visitor enters his details on the screen.
+    //2. Check if number exist and if it needs to be verified.
+    //3. if number exists, no need to verify. Store details and redirect to home page.
+    //4. if number does not exist, verify number. Store details and redirect to home page.
+
+    private FirebaseVisitorDao visitorDao;
     private OTPService otpService;
     private String sessionID;
     private boolean isNumberVerified;
@@ -56,7 +63,7 @@ public class ResidentialActivity extends AppCompatActivity {
     @OnClick(R.id.btnResidentialSubmit)
     void submit(){
         String phoneNumber = editTextPhoneNumber.getText().toString();
-        otpService.sendOTP(phoneNumber);
+        visitorDao.checkIfVisitorExists(phoneNumber);
     }
 
     @OnClick(R.id.btnResidentialVerifyPhoneNumber)
@@ -79,6 +86,23 @@ public class ResidentialActivity extends AppCompatActivity {
         visitorDao.writeRecord(visitor);
         Toast.makeText(this,"Visitor saved", Toast.LENGTH_LONG).show();
     }
+
+    private BroadcastReceiver phoneNumberVerifier = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "onReceive: phone number verifier intent received");
+            boolean doesNumberExist = intent.getBooleanExtra("phoneNumber", false);
+            if (doesNumberExist){
+                Log.d(TAG, "onReceive: Number exists");
+                Toast.makeText(ResidentialActivity.this,"This number exists in database. Not verifying",Toast.LENGTH_LONG).show();
+                storeVisitor();
+            }
+            else {
+                otpService.sendOTP(editTextPhoneNumber.getText().toString());
+                Toast.makeText(ResidentialActivity.this, "Sending OTP. Number not found in database.",Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 
     private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
         @Override
@@ -103,17 +127,19 @@ public class ResidentialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_residential);
-        visitorDao = new FirebaseVisitorDao();
+        visitorDao = new FirebaseVisitorDao(this);
         otpService = new TwoFactorOTP(this);
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         society = "None";
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, new IntentFilter("otp-service"));
+        LocalBroadcastManager.getInstance(this).registerReceiver(phoneNumberVerifier, new IntentFilter("visitor-exists"));
         ButterKnife.bind(this);
     }
 
     @Override
     protected void onDestroy() {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(phoneNumberVerifier);
         super.onDestroy();
     }
 }
--- app/src/main/java/entry/easyentry/dao/FirebaseVisitorDao.java
+++ app/src/main/java/entry/easyentry/dao/FirebaseVisitorDao.java
@@ -1,16 +1,29 @@
 package entry.easyentry.dao;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import entry.easyentry.models.Visitor;
 
 public class FirebaseVisitorDao implements Dao<Visitor> {
 
     private DatabaseReference database;
+    private Context context;
+    private static final String TAG = "FirebaseVisitorDao";
 
-    public FirebaseVisitorDao(){
+    public FirebaseVisitorDao(Context context){
         database = FirebaseDatabase.getInstance().getReference();
+        this.context = context;
     }
 
 
@@ -21,6 +34,8 @@ public class FirebaseVisitorDao implements Dao<Visitor> {
 
     @Override
     public Visitor readRecord() {
+        database = FirebaseDatabase.getInstance().getReference();
+        Query query = database.child("visitors").orderByChild("phoneNumber").equalTo("");
         return null;
     }
 
@@ -32,5 +47,29 @@ public class FirebaseVisitorDao implements Dao<Visitor> {
     @Override
     public void deleteRecord(Visitor visitor) {
 
+    }
+
+    public void checkIfVisitorExists(String phoneNumber){
+        Query query = database.child("visitors").orderByChild("phoneNumber").equalTo(phoneNumber);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Intent intent = new Intent("visitor-exists");
+                if (dataSnapshot!=null){
+                    Log.d(TAG, "onDataChange: DataSnapshot exists");
+                    intent.putExtra("phoneNumber",true);
+                }
+                else {
+                    Log.d(TAG, "onDataChange: DataSnapshot does not exists");
+                    intent.putExtra("phoneNumber",false);
+                }
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
